41e780794045198f5a165f2c4fbb06644dc2e791
anwar <>
2016-09-23 03:33:23 +0600

    First Commit

--- src/com/ak/da/capscreen/CapScreen.java
+++ src/com/ak/da/capscreen/CapScreen.java
@@ -5,10 +5,19 @@
  */
 package com.ak.da.capscreen;
 
+import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -27,6 +36,21 @@ public class CapScreen extends Application {
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    /**
+     * capture function
+     * @param fileName filename as a string
+     * @throws java.lang.Exception
+    */
+    
+    public void captureScreen(String fileName) throws Exception {
+        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice screen = env.getDefaultScreenDevice();
+        Robot robot = new Robot(screen);
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        BufferedImage img = robot.createScreenCapture(new Rectangle(0, 0, d.width, d.height));
+        ImageIO.write(img, "png", new File(fileName));
     }
 
     /**
