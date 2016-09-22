d1af151ea82a01b37b3578cb1699c793b5665cdc
anwar <user@user-PC>
2016-09-23 03:59:42 +0600

    Basic Functionality

--- src/com/ak/da/capscreen/CapScreen.java
+++ src/com/ak/da/capscreen/CapScreen.java
@@ -5,6 +5,7 @@
  */
 package com.ak.da.capscreen;
 
+import com.sun.java.swing.plaf.windows.WindowsTreeUI;
 import java.awt.Dimension;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
@@ -13,9 +14,15 @@ import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javax.imageio.ImageIO;
 
@@ -24,33 +31,64 @@ import javax.imageio.ImageIO;
  * @author user
  */
 public class CapScreen extends Application {
-    
+    public static Stage priStage;
     @Override
     public void start(Stage primaryStage) {
+        CapScreen.priStage = primaryStage;
+        //root group
+        Group root = new Group();
+        //scene
+        Scene scene = new Scene(root, 100, 100);
         
+        javafx.scene.shape.Rectangle rect = 
+                new javafx.scene.shape.Rectangle(100, 100);
         
-        StackPane root = new StackPane();
+        Button btnShot = new Button("Shot");
+        btnShot.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    captureScreen();
+                } catch (Exception ex) {
+                    //
+                }
+            }
+        });
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("CapScreen");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
     
     /**
      * capture function
-     * @param fileName filename as a string
      * @throws java.lang.Exception
     */
     
-    public void captureScreen(String fileName) throws Exception {
+    public void captureScreen() throws Exception {
         GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice screen = env.getDefaultScreenDevice();
         Robot robot = new Robot(screen);
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
         BufferedImage img = robot.createScreenCapture(new Rectangle(0, 0, d.width, d.height));
-        ImageIO.write(img, "png", new File(fileName));
+        File file = saveImage();
+        
+        if(file != null){
+            ImageIO.write(img, "png", file);
+        } else {
+            System.out.println("file not saved.");
+        }
+    }
+    
+    /**
+     * save dialog show
+     */
+    private File saveImage(){
+        FileChooser chooser = new FileChooser();
+        chooser.setInitialFileName("shot");
+        chooser.setTitle("Save File");
+        
+        return chooser.showSaveDialog(priStage);
     }
 
     /**
